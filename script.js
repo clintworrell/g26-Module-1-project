7c3afc872970b3f88b965ad859290d6dc1b24311
Clint Worrell <github@clintworrell.com>
2016-05-16 16:37:53 -0700

    Create Artist class

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var baseUrl = "https://api.spotify.com"
+var baseUrl = "https://api.spotify.com";
+
+var Artist = function(spotifyArtistJson) {
+  this.id = spotifyArtistJson.id;
+  this.name = spotifyArtistJson.name;
+  this.imageUrl;
+  for (var i = 0; i < spotifyArtistJson.images.length; i++) {
+    if (spotifyArtistJson.images[i].height <= 200 && spotifyArtistJson.images[i].width <= 200) {
+      this.imageUrl = spotifyArtistJson.images[i].url;
+    }
+  }
+};
 
 $('#artist-search-submit').click(function () {
   var artistName = $('#artist-search-input').val();
@@ -12,8 +23,10 @@ function searchForArtist(artistName) {
     success: function(results) {
       //toggle search area div off
       //toggle search results div on
-      var artistId = results.artists.items[0].id;
-      $('body').append(results.artists.items[0].name)
+      var firstReturnedArtist = results.artists.items[0];
+      var artist = new Artist(firstReturnedArtist);
+      // var artistId = results.artists.items[0].id;
+      // $('body').append(results.artists.items[0].name)
     }
   });
 }
