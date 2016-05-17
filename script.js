ce3be43c2b9b84056f2be580ad6291eda81d6705
Clint Worrell <github@clintworrell.com>
2016-05-17 10:47:38 -0700

    Add getRelatedArtists prototype

--- script.js
+++ script.js
@@ -18,8 +18,21 @@ Artist.prototype.renderCurrentArtist = function () {
   // $('#current-artist-image').append(`<img src=${this.imageUrl}>`);
 };
 
+Artist.prototype.getRelatedArtists = function () {
+  var getRelatedArtistsUrl = `/v1/artists/${this.id}/related-artists`;
+  $.ajax({
+    url: `${baseUrl}${getRelatedArtistsUrl}`,
+    method: "GET",
+    success: function(relatedArtists) {
+      $('#related-artist-1').append(relatedArtists.artists[0].name);
+      $('#related-artist-2').append(relatedArtists.artists[1].name);
+      $('#related-artist-3').append(relatedArtists.artists[2].name);
+    }
+  });
+}
+
 $('#artist-search-submit').click(function (e) {
-e.preventDefault();
+  e.preventDefault();
   var artistName = $('#artist-search-input').val();
   searchForArtist(artistName);
 });
@@ -34,6 +47,8 @@ function searchForArtist(artistName) {
       $('#artist-search-row').toggle();
       $('.artist-results-div').toggle();
       artist.renderCurrentArtist();
+      artist.getRelatedArtists();
+
       // var artistId = results.artists.items[0].id;
       // $('body').append(results.artists.items[0].name)
     }
